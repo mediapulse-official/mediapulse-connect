05adf311a2446450611d07be3b368ae6ed56a383
mediapulse-official <teamediapulse@gmail.com>
2025-12-05 16:24:48 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: './',
+  base: '/mediapulse-connect/',
   server: {
     host: "::",
     port: 8080,
